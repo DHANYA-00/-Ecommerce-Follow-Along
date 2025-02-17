d792b51089b4c5df6351b734385de316681b2352
Dhanya Lakshmi s s <dhanyalakshmi.s.s.06@gmail.com>
2025-02-17 11:53:28 +0530

    m-13 updated

--- frontend/src/pages/myProducts.jsx
+++ frontend/src/pages/myProducts.jsx
@@ -1,5 +1,6 @@
+
 import React, { useEffect, useState } from "react";
-import Product from "../components/product";
+import MyProduct from "../components/myproduct";
 
 
 export default function MyProducts() {
@@ -44,9 +45,12 @@ export default function MyProducts() {
             <h1 className="text-3xl text-center text-white py-6">My products</h1>
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4 p-4">
                 {products.map((product) => (
-                    <Product key={product._id} {...product} />
+                    <MyProduct key={product._id} {...product} />
                 ))}
             </div>
         </div>
     );
 }
+
+
+
